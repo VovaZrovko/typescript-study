bd06e04329dd0c521e8f1a688591b337c029b24d
VovaZrovko <vovazrovko380969842477@gmail.com>
2025-01-22 17:46:52 +0200

    custom type with generic

--- exercises/ex9/ex9.ts
+++ exercises/ex9/ex9.ts
@@ -33,12 +33,10 @@ const users: User[] = [
     { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' }
 ];
 
-export type ApiResponse<T> = unknown;
-
-type AdminsApiResponse = (
+export type ApiResponse<T> = (
     {
         status: 'success';
-        data: Admin[];
+        data: T;
     } |
     {
         status: 'error';
@@ -46,39 +44,30 @@ type AdminsApiResponse = (
     }
 );
 
-export function requestAdmins(callback: (response: AdminsApiResponse) => void) {
+export function requestAdmins(callback: (response: ApiResponse<Admin[]>) => void) {
     callback({
         status: 'success',
         data: admins
     });
 }
 
-type UsersApiResponse = (
-    {
-        status: 'success';
-        data: User[];
-    } |
-    {
-        status: 'error';
-        error: string;
-    }
-);
-
-export function requestUsers(callback: (response: UsersApiResponse) => void) {
+export function requestUsers(callback: (response: ApiResponse<User[]>) => void) {
     callback({
         status: 'success',
         data: users
     });
 }
 
-export function requestCurrentServerTime(callback: (response: unknown) => void) {
+export function requestCurrentServerTime(callback: (response: ApiResponse<number>) => void) {
     callback({
         status: 'success',
         data: Date.now()
     });
 }
-
-export function requestCoffeeMachineQueueLength(callback: (response: unknown) => void) {
+// ApiResponse<number> here I was wandering should I use number or never as in callback we send an error
+// due to test on site here should be a number. But another question should it be so??? 
+// If we know that we pass error so we should pass ApiErrorResponse.
+export function requestCoffeeMachineQueueLength(callback: (response: ApiResponse<number>) => void) {
     callback({
         status: 'error',
         error: 'Numeric value has exceeded Number.MAX_SAFE_INTEGER.'
