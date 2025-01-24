6b49746496b779d0193d70dd5c284a787960b3ef
VovaZrovko <vovazrovko380969842477@gmail.com>
2025-01-24 18:03:15 +0200

    declared types for index.js

--- exercises/ex11/module/index.d.ts
+++ exercises/ex11/module/index.d.ts
@@ -1,3 +1,8 @@
 declare module './module/index' {
-    
+    type strUtilType = (value: string) => string;
+    export const strReverse: strUtilType;
+    export const strToLower: strUtilType;
+    export const strToUpper: strUtilType;
+    export const strRandomize: strUtilType;
+    export const strInvertCase: strUtilType;
 }
