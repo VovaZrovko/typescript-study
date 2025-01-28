f3dec70be89b91cbd3c65a07673c6428c7810ef0
VovaZrovko <vovazrovko380969842477@gmail.com>
2025-01-28 15:25:05 +0200

    provided type declaration for index.js

--- exercises/ex12/index.d.ts
+++ exercises/ex12/index.d.ts
@@ -1,3 +1,11 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type getValueType<T> = (value: T) => number;
+    type comparatorType<T> = (value1: T, value2: T) => number;
+    export function getMaxIndex<T>(input: T[], comparator: comparatorType<T>): number;
+    export function getMaxElement<T>(input: T[], comparator: comparatorType<T>): null | T;
+    export function getMinIndex<T>(input: T[], comparator: comparatorType<T>): number;
+    export function getMinElement<T>(input: T[], comparator: comparatorType<T>): null | T;
+    export function getMedianIndex<T>(input: T[], comparator: comparatorType<T>): number;
+    export function getMedianElement<T>(input: T[], comparator: comparatorType<T>): null | T;
+    export function getAverageValue<T>(input: T[], getValue: getValueType<T>): null | number;
 }
