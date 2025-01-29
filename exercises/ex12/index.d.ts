339b7a1bc000f4433a5a5ccda3f64814f8ebdcc1
VovaZrovko <vovazrovko380969842477@gmail.com>
2025-01-29 13:09:01 +0200

    inline generic. Extra task

--- exercises/ex12/index.d.ts
+++ exercises/ex12/index.d.ts
@@ -1,11 +1,16 @@
 declare module 'stats' {
     type getValueType<T> = (value: T) => number;
     type comparatorType<T> = (value1: T, value2: T) => number;
-    export function getMaxIndex<T>(input: T[], comparator: comparatorType<T>): number;
-    export function getMaxElement<T>(input: T[], comparator: comparatorType<T>): null | T;
-    export function getMinIndex<T>(input: T[], comparator: comparatorType<T>): number;
-    export function getMinElement<T>(input: T[], comparator: comparatorType<T>): null | T;
-    export function getMedianIndex<T>(input: T[], comparator: comparatorType<T>): number;
-    export function getMedianElement<T>(input: T[], comparator: comparatorType<T>): null | T;
+    type nullOrSomethingFunctionType = <T>(input: T[], comparator: comparatorType<T>) => null | T;
+    type numberFunctionType = <T>(input: T[], comparator: comparatorType<T>) => number;
+    // tried type numberFunctionType<T> but then when using it I should have put a proper type to numberFunctionType
+    // could use any but the task to avoid it.
+
+    export const getMaxIndex: numberFunctionType;
+    export const getMaxElement: nullOrSomethingFunctionType;
+    export const getMinIndex: numberFunctionType;
+    export const getMinElement: nullOrSomethingFunctionType;
+    export const getMedianIndex: numberFunctionType;
+    export const getMedianElement: nullOrSomethingFunctionType;
     export function getAverageValue<T>(input: T[], getValue: getValueType<T>): null | number;
 }
