d0cfd225ca6c1f75d7130cefd6b5b8d8d25438a9
VovaZrovko <vovazrovko380969842477@gmail.com>
2025-01-30 11:58:46 +0200

    task 15 created. task 14 postponed as the hardest task for now

---
+++ exercises/ex15/ex15.ts
@@ -0,0 +1,31 @@
+/* link: https://typescript-exercises.github.io/#exercise=15&file=%2Findex.ts
+Exercise:
+
+    Here is a library which helps manipulating objects.
+    We tried to write type annotations and we failed.
+    Please help!
+
+*/
+
+export class ObjectManipulator {
+
+    constructor(protected obj) {}
+
+    public set(key, value) {
+        return new ObjectManipulator({...this.obj, [key]: value});
+    }
+
+    public get(key) {
+        return this.obj[key];
+    }
+
+    public delete(key) {
+        const newObj = {...this.obj};
+        delete newObj[key];
+        return new ObjectManipulator(newObj);
+    }
+
+    public getObject() {
+        return this.obj;
+    }
+}
